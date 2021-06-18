944cce29d563d6db5f7144cd074df4df57cfda82
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-06-18 18:08:15 +0530

    Update traffic_sign.py

--- traffic_sign.py
+++ traffic_sign.py
@@ -37,7 +37,6 @@ print(data.shape, labels.shape)
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2, random_state=42)
 
-print(X_train)
 print("x_train shape:", X_train.shape)
 print("test shape:", X_test.shape)
 print("y train:",y_train.shape)
@@ -46,3 +45,19 @@ print("y test:",y_test.shape)
 y_train = to_categorical(y_train, 43)
 y_test = to_categorical(y_test, 43)
 
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu',input_shape= X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43,activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+              
